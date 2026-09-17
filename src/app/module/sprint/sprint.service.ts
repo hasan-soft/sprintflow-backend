6b92b8aa585270a811509c552fe6153da232bc3d
Hasan Bhuiyan <hasanbhuiyan503@gmail.com>
2026-09-17 07:15:13 +0600

    sprint.interface.ts file added and service.ts file modify

--- src/app/module/sprint/sprint.service.ts
+++ src/app/module/sprint/sprint.service.ts
@@ -1,49 +1,44 @@
-import { SprintStatus } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
+import type { SprintStatus } from "../../../generated/prisma/client";
+import type { ICreateSprintPayload } from "./sprint.interface";
 
-const createSprint = async (payload: {
-	name: string;
-	projectId: string;
-	startDate: string;
-	endDate: string;
-	status?: SprintStatus;
-}) => {
-	const sprint = await prisma.sprint.create({
-		data: {
-			name: payload.name,
-			projectId: payload.projectId,
-			startDate: new Date(payload.startDate),
-			endDate: new Date(payload.endDate),
-			status: payload.status || SprintStatus.UPCOMING,
-		},
-	});
-	return sprint;
+const createSprint = async (payload: ICreateSprintPayload) => {
+  const sprint = await prisma.sprint.create({
+    data: {
+      name: payload.name,
+      projectId: payload.projectId,
+      startDate: new Date(payload.startDate),
+      endDate: new Date(payload.endDate),
+      status: payload.status || ("UPCOMING" as SprintStatus),
+    },
+  });
+  return sprint;
 };
 
 const getSprintsByProject = async (projectId: string) => {
-	const sprints = await prisma.sprint.findMany({
-		where: { projectId },
-		include: {
-			tasks: { where: { isDeleted: false } },
-		},
-		orderBy: { createdAt: "desc" },
-	});
-	return sprints;
+  const sprints = await prisma.sprint.findMany({
+    where: { projectId },
+    include: {
+      tasks: { where: { isDeleted: false } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+  return sprints;
 };
 
 const updateSprintStatus = async (id: string, status: SprintStatus) => {
-	const isSprintExist = await prisma.sprint.findUnique({ where: { id } });
-	if (!isSprintExist) throw new Error("Sprint not found");
+  const isSprintExist = await prisma.sprint.findUnique({ where: { id } });
+  if (!isSprintExist) throw new Error("Sprint not found");
 
-	const updatedSprint = await prisma.sprint.update({
-		where: { id },
-		data: { status },
-	});
-	return updatedSprint;
+  const updatedSprint = await prisma.sprint.update({
+    where: { id },
+    data: { status },
+  });
+  return updatedSprint;
 };
 
 export const SprintService = {
-	createSprint,
-	getSprintsByProject,
-	updateSprintStatus,
+  createSprint,
+  getSprintsByProject,
+  updateSprintStatus,
 };
